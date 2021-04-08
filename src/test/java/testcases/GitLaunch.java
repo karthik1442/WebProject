e125189b3e0e4c3165649c32cba848a94ece4f73
kutala1442 <kutalakarthik@gmail.com>
2021-04-07 21:21:17 -0600

    git/timesheet practice code

---
+++ src/test/java/testcases/GitLaunch.java
@@ -0,0 +1,36 @@
+package testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GitLaunch {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+	    driver.get("https://github.com/");
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+	    driver.findElement(By.xpath("//a[normalize-space()='Sign in']")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("//input[@id='login_field']")).sendKeys("kutalakarthik@gmail.com");
+	    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Ap29bx1442@");
+	    driver.findElement(By.xpath("//input[@name='commit']")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("//*[@id=\"repos-container\"]/ul/li[1]/div/a/span[1]")).click();
+	    Thread.sleep(2000);
+	    driver.close();
+	    
+	   
+		
+	}
+ 
+}
---
+++ src/test/java/testcases/TimeSheet.java
@@ -0,0 +1,33 @@
+package testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TimeSheet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.sageintacct.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(30));
+		driver.findElement(By.xpath("//a[normalize-space()='Sign in']")).click();
+		driver.findElement(By.xpath("//input[@id='company']")).clear();
+		driver.findElement(By.xpath("//input[@id='company']")).sendKeys("prokarma");
+		driver.findElement(By.xpath("//input[@id='login']")).sendKeys("Kkutala");
+		driver.findElement(By.xpath("//input[@id='passwd']")).sendKeys("Dady1234@");
+		driver.findElement(By.xpath("//input[@id='retbutton']")).click();
+		driver.close();
+	
+	
+	}
+
+}
